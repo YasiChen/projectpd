ea83f6406248abd73c2fec587b2b64ecea7811c4
chenyasi <chenyasi@chenyaside-MacBook-Pro.local>
2020-07-27 15:26:10 -0400

    Initial commit

---
+++ Pokemon.py
@@ -0,0 +1,117 @@
+# import pandas as pd
+# df =pd.read_csv("/Users/chenyasi/Documents/PythonPractice/pokemon_data.csv")
+# print(df)
+
+import pprint
+pprint.pprint()
+
+import pandas as pd
+import numpy as np
+df =pd.read_csv("/Users/chenyasi/Documents/PythonPractice/pokemon_data.csv")
+print(df.head(10)) #show the first 10
+
+# read headers
+print(df.columns)
+# read each column
+print(df[['Name','Type 1']][0:5])
+# read each row
+print(df.head(4))
+print(df.iloc[0:4])
+print(df.iloc[2,1])
+
+for index,row in df.iterrows():
+    print(index,row['Name'])
+
+df.loc[df['Type 1']=="Fire"]
+
+
+# describe
+df.describe()
+df.sort_values(['Type 1', 'HP'],ascending=[True,False])
+
+# col sum
+df['Total'] = df['HP']+df['Attack']+df['Defense']
+
+# drop col
+df.drop(columns=['Total'])
+
+df['Total']=df.iloc[:,4:10].sum(axis=1)
+
+# arrange the order
+cols=list(df.columns.values)
+df[cols[0:4]+[cols[-1]]+cols[4:12]]
+# or
+df=df[['HP','Name','Type 1']]
+
+# get rid of the index
+df.to_csv('modified.csv',index=False)
+
+df.to_excel('modified.elsx',index=False)
+
+df.to_csv('modified.txt', index=False, sep='\t')
+
+# filter data
+newdf = df.loc[(df['Type1']=='Grass') & (df['Type 2']=='Aggressive')]
+
+newdf=newdf.reset_index(drop=True, inplace=True)
+
+# like, contains
+df.loc[df['Name'].str.contains('Mega')]
+# not contains
+df.loc[~df['Name'].str.contains('Mega')]
+
+
+# ignore the capital, contains fire or grass
+import re
+df.loc[df['Type 1'].str.contains('Fire|Grass', flags=re.I, regex=True)]
+
+# the name start with pi
+df.loc[df['Name'].str.contains('^pi[a-z]', flags=re.I, regex=True)]
+
+
+# conditional change
+df.loc[df['Type 1']=='Fire','Type 1']='Flamer'
+df.loc[df['Type 1']=='Fire',['Gerenation','Total']]=['Flamer', True]
+
+
+# groupby
+df.groupby('Type 1').mean().sort_values('Defense',ascending=False)
+df.groupby('Type 1').sum()
+df.groupby('Type 1').count()#not count null
+# clear up, only bring up the count
+df['count']=1
+df.groupby(['Type 1','Type 2']).count()['count']
+
+# read trunck at a time
+df= pd.read_csv("/Users/chenyasi/Documents/PythonPractice/pokemon_data.csv", chunksize=1000)
+
+
+
+for df in pd.read_csv("/Users/chenyasi/Documents/PythonPractice/pokemon_data.csv", chunksize=5):
+    print("Chunk df")
+    print(df)
+
+# big data set analysis
+new_df=pd.DataFrame(columns=df.columns)
+
+for df in pd.read_csv("/Users/chenyasi/Documents/PythonPractice/pokemon_data.csv", chunksize=5):
+    results=df.groupby(['Type 1']).count()
+    new_df=pd.concat(new_df,results)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Sale_Analysis.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import os
+
+# merging all the data into a single file
+# df=pd.read_csv('/Users/chenyasi/Documents/PythonPractice/Pandas-Data-Science-Tasks-master/SalesAnalysis/Sales_Data/Sales_April_2019.csv')
+files=[file for file in os.listdir('/Users/chenyasi/Documents/PythonPractice/Pandas-Data-Science-Tasks-master/SalesAnalysis/Sales_Data')]
+
+all_months_data=pd.DataFrame()
+
+for file in files:
+    df=pd.read_csv('/Users/chenyasi/Documents/PythonPractice/Pandas-Data-Science-Tasks-master/SalesAnalysis/Sales_Data/'+file)
+    all_months_data = pd.concat([all_months_data, df])
+
+all_months_data.to_csv("all_data.csv", index=False)
+all_months_data.head()
+
+
+all_data=pd.read_csv("all_data.csv")
+all_data.head()
+
+
+# argument the data with extra col
+# adding the month
+
+all_data['Month']= all_data['Order Date'].str[0:2]
+# define the month type
+# all_data['Month']=all_data['Month'].astype('int32')
+# not working coz of the messy data has things other than date
+all_data['Month'].head(500)
+
+# drop rows of NAN
+nan_df= all_data[all_data.isna().any(axis=1)]
+all_data=all_data.dropna(how='all')
+all_data
+all_data['Month'].unique()
+all_data=all_data[all_data['Month']!='Or']
+
+all_data['Month']=all_data['Month'].astype('int32')
+
+
+# sales value
+# collect type calculation first
+all_data['Quantity Ordered'] = pd.to_numeric(all_data['Quantity Ordered'])
+all_data['Price Each'] = pd.to_numeric(all_data['Price Each'])
+all_data['Sales'] = all_data['Quantity Ordered']*all_data['Price Each']
+all_data.head()
+
+results=all_data.groupby('Month').sum()
+
+import matplotlib.pyplot as plt
+
+# plot monthly sales
+months=range(1,13)
+plt.bar(months, results['Sales'])
+plt.xticks(months)
+plt.ylabel('Sales in USD')
+plt.xlabel('Month Number')
+plt.show()
+
+# demographic
+# extract the city
+# use apply()
+def get_city(address):
+    return  address.split(',')[1]
+
+
+all_data['City']=all_data['Purchase Address'].apply(lambda x: x.split(',')[1])
+all_data.head()
+all_data['City'].unique()
+# it can also be trouble if two city named the same but actually two places
+
+# apply lambda x , can apply other functions as well
+all_data['City']=all_data['Purchase Address'].apply(lambda x: get_city(x))
+
+
+
+city_sales= all_data.groupby('City').sum()
